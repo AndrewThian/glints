da94401bc6676edd41c9b786fdbc66db535596d2
AndrewThian <andrewbobbythian@gmail.com>
2018-12-02 22:16:28 +0800

    matrix-rotation complete

--- matrix-rotation.js
+++ matrix-rotation.js
@@ -2,24 +2,36 @@
 // 2. rotate each of those arrays by shifting and pushing
 // 3. fill a grid back up with the unwrapped rings
 
+export const matrixRotation = (rows, cols, rotations, grid) => {
+    const store = traverseSpiral(rows, cols, grid)
+    const rings = store.values()
+    // rotate each ring
+    for (const ring of rings) {
+        rotate(ring, rotations)
+    }
+    // fill grid with rings
+    const rotated = fillGrid(rows, cols, store)
+    print(rotated)
+    return rotated
+}
+
 /**
- * @param {*} width max width of the grid
- * @param {*} height max height of the grid
+ * @param {*} row max rows of the grid
+ * @param {*} col max cols of the grid
  * @param {*} grid grid[][]
  */
-export function traverseSpiral(width, height, grid) {
-    if (Math.min(width, height) % 2 !== 0) {
+export function traverseSpiral(rows, cols, grid) {
+    if (Math.min(rows, cols) % 2 !== 0) {
         return new Map();
     }
 
     let rowStart = 0
     let colStart = 0
 
-    // normalize from numerical to index numerical
     // last row should be the maximum height of the grid
-    let rowEnd = height - 1
+    let rowEnd = rows - 1
     // last col should be the maximum width of the grid
-    let colEnd = width - 1
+    let colEnd = cols - 1
 
     const store = new Map()
     let currentRing = []
@@ -72,6 +84,67 @@ export function traverseSpiral(width, height, grid) {
     return store
 }
 
+export function fillGrid(row, col, store) {
+    let grid = []
+    for (let i = 0; i < row; i++) {
+        grid.push([])
+    }
+
+    let rowStart = 0
+    let colStart = 0
+
+    let rowEnd = row - 1
+    let colEnd = col - 1
+
+    let ringCounter = 1
+    let ringIndex = 0
+
+    while (rowStart < rowEnd && colStart < colEnd) {
+        // first row move right
+        // grid[x][y++]
+        for (let i = colStart; i <= colEnd; i++) {
+            grid[rowStart][i] = store.get(ringCounter)[ringIndex]
+            ringIndex++
+        }
+        rowStart++
+
+        // last column move down
+        // grid[x--][y]
+        for (let i = rowStart; i <= rowEnd; i++) {
+            grid[i][colEnd] = store.get(ringCounter)[ringIndex]
+            ringIndex++
+        }
+        colEnd--
+
+        // last row move right
+        // grid[x][y--]
+        for (let i = colEnd; i >= colStart; i--) {
+            // console.log("bottom row: ", grid[rowEnd][i])
+            grid[rowEnd][i] = store.get(ringCounter)[ringIndex]
+            ringIndex++
+        }
+        // start traversal from one row up to avoid repeats
+        rowEnd--
+
+        // first column move up
+        // grid[x--][y]
+        for (let i = rowEnd; i >= rowStart; i--) {
+            // console.log("left col: ", grid[i][colStart])
+            grid[i][colStart] = store.get(ringCounter)[ringIndex]
+            ringIndex++
+        }
+        // start next ring traverse from one col right
+        colStart++
+
+        // next ring in store
+        ringCounter++
+        // reset ring index
+        ringIndex = 0
+    }
+
+    return grid
+}
+
 export function rotate(arr, times) {
     for (let i = 0; i < times; i++) {
         const ele = arr.shift()
@@ -84,13 +157,19 @@ export function createGrid(rows, cols) {
     const row = rows;
     let grid = [];
     let counter = 1;
-    for (let i = 0; i < col; i++) {
+    for (let i = 0; i < row; i++) {
         let column = [];
         grid[i] = column;
-        for (let j = 0; j < row; j++) {
+        for (let j = 0; j < col; j++) {
             column.push(counter);
             counter++;
         }
     }
     return grid;
+}
+
+function print (grid) {
+    grid.map((row) => {
+        console.log(...row)
+    })
 }
