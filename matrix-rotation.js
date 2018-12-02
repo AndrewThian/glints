20aeb8cd4453123e458578b7eaecae81343bd62e
AndrewThian <andrewbobbythian@gmail.com>
2018-12-02 21:12:21 +0800

    rotate individual array and create grid

--- matrix-rotation.js
+++ matrix-rotation.js
@@ -1,5 +1,8 @@
+// 1. we want to traverse the grid ring by ring, to unwrap all the rings into arrays
+// 2. rotate each of those arrays by shifting and pushing
+// 3. fill a grid back up with the unwrapped rings
+
 /**
- * we want to traverse the grid ring by ring
  * @param {*} width max width of the grid
  * @param {*} height max height of the grid
  * @param {*} grid grid[][]
@@ -18,14 +21,16 @@ export function traverseSpiral(width, height, grid) {
     // last col should be the maximum width of the grid
     let colEnd = width - 1
 
-    const rings = new Map()
-    const currentRing = []
+    const store = new Map()
+    let currentRing = []
+    let ringCounter = 1
     // while not done with the spiral
     while(rowStart < rowEnd && colStart < colEnd) {
         // first row move right
         // grid[x][y++]
         for (let i = colStart; i <= colEnd; i++) {
-            console.log("top row: ", grid[rowStart][i])
+            // console.log("top row: ", grid[rowStart][i])
+            currentRing.push(grid[rowStart][i])
         }
         // start traversing from one row down to avoid repeats
         rowStart++
@@ -33,7 +38,8 @@ export function traverseSpiral(width, height, grid) {
         // last column move down
         // grid[x--][y]
         for (let i = rowStart; i <= rowEnd; i++) {
-            console.log("right col: ", grid[i][colEnd])
+            // console.log("right col: ", grid[i][colEnd])
+            currentRing.push(grid[i][colEnd])
         }
         // start traversing from one col left to avoid repeats
         colEnd--
@@ -41,7 +47,8 @@ export function traverseSpiral(width, height, grid) {
         // last row move right
         // grid[x][y--]
         for (let i = colEnd; i >= colStart; i--) {
-            console.log("bottom row: ", grid[rowEnd][i])
+            // console.log("bottom row: ", grid[rowEnd][i])
+            currentRing.push(grid[rowEnd][i])
         }
         // start traversal from one row up to avoid repeats
         rowEnd--
@@ -49,9 +56,41 @@ export function traverseSpiral(width, height, grid) {
         // first column move up
         // grid[x--][y]
         for (let i = rowEnd; i >= rowStart; i--) {
-            console.log("left col: ", grid[i][colStart])
+            // console.log("left col: ", grid[i][colStart])
+            currentRing.push(grid[i][colStart])
         }
         // start next ring traverse from one col right
         colStart++
+        
+        // store current ring into store
+        store.set(ringCounter, currentRing)
+        // clear current ring
+        currentRing = []
+        // increment ring counter
+        ringCounter++
     }
+    return store
+}
+
+export function rotate(arr, times) {
+    for (let i = 0; i < times; i++) {
+        const ele = arr.shift()
+        arr.push(ele)
+    }
+}
+
+export function createGrid(rows, cols) {
+    const col = cols;
+    const row = rows;
+    let grid = [];
+    let counter = 1;
+    for (let i = 0; i < col; i++) {
+        let column = [];
+        grid[i] = column;
+        for (let j = 0; j < row; j++) {
+            column.push(counter);
+            counter++;
+        }
+    }
+    return grid;
 }
--- matrix-rotation.spec.js
+++ matrix-rotation.spec.js
@@ -1,12 +1,63 @@
-import { traverseSpiral } from "./matrix-rotation"
+import { traverseSpiral, rotate, createGrid } from "./matrix-rotation"
 
 describe("#traverseSpiral", () => {
-    const grid = [
-        [1, 2, 3, 4],
-        [5, 6, 7, 8],
-        [9, 10, 11, 12],
-        [13, 14, 15, 16]
-    ]
+    it("4x4: should return a map of 2 rings", () => {
+        const grid = [
+            [1, 2, 3, 4],
+            [5, 6, 7, 8],
+            [9, 10, 11, 12],
+            [13, 14, 15, 16]
+        ]
+        const map = new Map()
+        map.set(1, [1,2,3,4,8,12,16,15,14,13,9,5])
+        map.set(2, [6,7,11,10])
+    
+        expect(traverseSpiral(4, 4, grid)).toEqual(map)
+    })
+    it("2x4: should return a map of 1 ring", () => {
+        const grid = [
+            [1,2],
+            [3,4],
+            [5,6],
+            [7,8]
+        ]
+        const map = new Map()
+        map.set(1, [1,2,4,6,8,7,5,3])
+    
+        expect(traverseSpiral(2, 4, grid)).toEqual(map)
+    })
+})
 
-    traverseSpiral(4, 4, grid)
+describe("#rotate", () => {
+    it("should rotate an array 2 times", () => {
+        const testArr = [1,2,3,4]
+        rotate(testArr, 2)
+        expect(testArr).toEqual([3, 4, 1, 2])
+    })
+    it("should rotate an array 3 times", () => {
+        const testArr = [1,2,3,4,5,6]
+        rotate(testArr, 3)
+        expect(testArr).toEqual([4,5,6,1,2,3])
+    })
+})
+
+describe("#createGrid", () => {
+    it("should create 2x4 grid", () => {
+        const grid = [
+            [1,2],
+            [3,4],
+            [5,6],
+            [7,8]
+        ]
+        expect(createGrid(2, 4)).toEqual(grid)
+    })
+    it("should create 4x4 grid", () => {
+        const grid = [
+            [1, 2, 3, 4],
+            [5, 6, 7, 8],
+            [9, 10, 11, 12],
+            [13, 14, 15, 16]
+        ]
+        expect(createGrid(4, 4)).toEqual(grid)
+    })
 })
